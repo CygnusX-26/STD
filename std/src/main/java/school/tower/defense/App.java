bcba7ca0d4c993dd5821b1b1d83cfcd46faa429d
CygnusX-26 <42686923+CygnusX-26@users.noreply.github.com>
2022-05-05 22:20:53 -0700

    updates

--- std/src/main/java/school/tower/defense/App.java
+++ std/src/main/java/school/tower/defense/App.java
@@ -7,9 +7,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -21,20 +27,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        StackPane root  = new StackPane();
-        StackPane game = new StackPane();
-        game.setId("game");
-        root.setId("pane");
-        Button btn = new Button();
-        btn.setText("Continue");
-        btn.setOnAction(value ->  {
-          scene.setRoot(game);
-           System.out.println("Continue");
-        });
-        root.getChildren().add(btn);
-        scene = new Scene(root, 640, 480, Color.BLACK);
+        loadMenu();
         URL url = this.getClass().getResource("style.css");
-        System.out.println(url);
         String css = url.toExternalForm();
         scene.getStylesheets().add(css);
         stage.setScene(scene);
@@ -57,6 +51,45 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void loadMenu() throws IOException {
+        StackPane root  = new StackPane();
+        StackPane game = new StackPane();
+        game.setId("game");
+        root.setId("pane");
+        Button btn = new Button();
+        Text text = new Text("Student Tower Defense");
+        text.setTranslateY(-150);
+        btn.setTranslateX(-150);
+        text.setId("menutext");
+        BackgroundImage backgroundImage = new BackgroundImage( new Image( getClass().getResource("Map/start.png").toExternalForm()), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, true, false));
+        btn.setBackground( new Background( backgroundImage ));
+        btn.setMaxSize(100, 100);
+        btn.setOnAction(value ->  {
+            scene.setRoot(game);
+            btn.setDisable(true);
+        });
+        root.getChildren().add(btn);
+        root.getChildren().add(text);
+        scene = new Scene(root, 640, 480, Color.BLACK);
     }
 
 }
