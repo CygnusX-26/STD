894d173f49032b31d43669018687f165f85f1cd3
CygnusX-26 <42686923+CygnusX-26@users.noreply.github.com>
2022-05-08 15:45:18 -0700

    updated menu

--- std/src/main/java/school/tower/defense/App.java
+++ std/src/main/java/school/tower/defense/App.java
@@ -2,6 +2,8 @@ package school.tower.defense;
 
 import java.io.IOException;
 import java.net.URL;
+import javafx.animation.Animation;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,6 +20,8 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 /**
  * JavaFX App
@@ -25,16 +29,19 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     private static Scene scene;
+    private MediaPlayer menuPlayer;
+    private Stage stage;
 
     @Override
     public void start(Stage stage) throws IOException {
         loadMenu();
+        this.stage = stage;
         URL url = this.getClass().getResource("style.css");
         String css = url.toExternalForm();
         scene.getStylesheets().add(css);
-        stage.setScene(scene);
-        stage.setTitle("Student Tower Defense");
-        stage.show();
+        this.stage.setScene(scene);
+        this.stage.setTitle("Student Tower Defense");
+        this.stage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -52,30 +59,44 @@ public class App extends Application {
     public void loadMenu() throws IOException {
         StackPane root  = new StackPane();
 
+        TranslateTransition fulkTransition = new TranslateTransition();
+        TranslateTransition kwongTransition = new TranslateTransition();
         Button startButton = new Button();
         Button instructions = new Button();
-        BackgroundImage startImage = new BackgroundImage( new Image( getClass().getResource("Map/start.png").toExternalForm()), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false));
-        BackgroundImage instructionsImage = new BackgroundImage( new Image( getClass().getResource("Map/Instructions.png").toExternalForm()), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, true, false));
+        BackgroundImage startImage = new BackgroundImage( new Image( getClass().getResource("Map/start.png").toExternalForm()), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(150, 75, false, false, true, false));
+        BackgroundImage instructionsImage = new BackgroundImage( new Image( getClass().getResource("Map/Instructions.png").toExternalForm()), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(150, 75, true, true, true, false));
         Text text = new Text("Student Tower Defense");
         Image fulk = new Image(getClass().getResource("Map/fulk.PNG").toExternalForm());
         ImageView FULK = new ImageView(fulk);
         Image kwong = new Image(getClass().getResource("Map/Kwong.PNG").toExternalForm());
         ImageView KWONG = new ImageView(kwong);
+        Media menuMusic = new Media(getClass().getResource("Music/awesomeness.wav").toExternalForm());
+        menuPlayer = new MediaPlayer(menuMusic);
 
         root.setId("pane");
         text.setId("menutext");
 
         text.setTranslateY(-150);
         startButton.setBackground( new Background( startImage ));
-        startButton.setScaleX(7);
-        startButton.setScaleY(7);
+        startButton.setTranslateY(-25);
+        startButton.setMaxWidth(150);
+        startButton.setMaxHeight(75);
         instructions.setBackground(new Background( instructionsImage ));
-        instructions.setScaleX(7);
-        instructions.setScaleY(7);
+        instructions.setMaxWidth(150);
+        instructions.setMaxHeight(75);
         instructions.setTranslateY(50);
         FULK.setTranslateX(200);
         KWONG.setTranslateX(-200);
-
+        fulkTransition.setNode(FULK);
+        fulkTransition.setByY(50);
+        fulkTransition.setAutoReverse(true);
+        fulkTransition.setCycleCount(Animation.INDEFINITE);
+        kwongTransition.setNode(KWONG);
+        kwongTransition.setByY(50);
+        kwongTransition.setAutoReverse(true);
+        kwongTransition.setCycleCount(Animation.INDEFINITE);
+        menuPlayer.setAutoPlay(true);
+        menuPlayer.setCycleCount(MediaPlayer.INDEFINITE);
 
         startButton.setOnAction(value ->  {
             try {
@@ -102,6 +123,11 @@ public class App extends Application {
         root.getChildren().add(FULK);
         root.getChildren().add(KWONG);
         root.getChildren().add(instructions);
+
+        fulkTransition.play();
+        kwongTransition.play();
+        menuPlayer.play();
+
         scene = new Scene(root, 640, 480, Color.BLACK);
     }
 
@@ -113,6 +139,12 @@ public class App extends Application {
     }
 
     public void loadGame() throws IOException {
+        menuPlayer.stop();
+        Media gameMusic = new Media(getClass().getResource("Music/game.mp4").toExternalForm());
+        menuPlayer = new MediaPlayer(gameMusic);
+        menuPlayer.setVolume(0.5);
+        menuPlayer.play();
+        stage.setMaximized(true);
         StackPane game = new StackPane();
         game.setId("game");
         scene.setRoot(game);
