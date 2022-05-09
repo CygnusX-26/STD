0f4ca5201622587ba8944d88271dcbdcb8715962
firsttobebear <axillarystudios@gmail.com>
2022-05-09 12:47:09 -0700

    new stuff

--- std/src/main/java/school/tower/defense/Templates/Tower.java
+++ std/src/main/java/school/tower/defense/Templates/Tower.java
@@ -1,11 +1,12 @@
 package school.tower.defense.Templates;
 
-import school.tower.defense.Classes.Upgrade;
+import school.tower.defense.Classes.*;
 
 public abstract class Tower {
-    public abstract void place(); //Will place the tower and deduct the cost
+    public abstract void place(Location location); //Will place the tower and deduct the cost
     public abstract void sell(); //Will sell the tower and add the cost
     public abstract void upgrade(); //Will upgrade the tower and deduct the cost
     public abstract int getCost(); //Will return the cost of the tower
     public abstract Upgrade getUpgrade(); //Will return the damage of the tower
+    public abstract void setTile(Tile tile); //Will set the tile that the tower is on
 }
