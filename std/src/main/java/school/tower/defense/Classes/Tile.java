66aee1794ba877f42a2a7ea4fa9e871e8cfc30a7
cnewton678@student.fuhsd.org <cnewton678@student.fuhsd.org>
2022-05-12 11:57:34 -0700

    tile javadoc

--- std/src/main/java/school/tower/defense/Classes/Tile.java
+++ std/src/main/java/school/tower/defense/Classes/Tile.java
@@ -14,24 +14,48 @@ public class Tile {
         this.tower = null;
     }
 
+    
+    /** 
+     * Returns the location of the tile
+     * @return Location of the location
+     */
     public Location getLocation() {
         return location;
     }
 
+    
+    /** 
+     * Returns if the tile is empty (used for placeing new teacher down)
+     * @return boolean of if the tile is empty
+     */
     public boolean isEmpty() {
         return !isOccupied;
     }
 
+    
+    /** 
+     * Returns the tower that is inside the tile
+     * @return Tower that is in the tile
+     */
     public Tower occupiedBy() {
         return tower;
     }
 
+    
+    /** 
+     * Puts a tower inside the tile and changes the private variables as appropriate
+     * @param tower the tower to be inserted into the tile
+     */
     public void setTower(Tower tower) {
         this.tower = tower;
         this.tower.setTile(this);
         isOccupied = true;
     }
 
+    /** 
+     * Removes the tower inside the tile and changes the private variables as appropriate
+     * @return void
+     */
     public void removeTower() {
         this.tower.setTile(null);
         this.tower = null;
