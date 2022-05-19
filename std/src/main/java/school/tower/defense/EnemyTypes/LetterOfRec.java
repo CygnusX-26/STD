3000fd8ff729b807c8f58b7ee37b2531c8461394
cnewton678@student.fuhsd.org <cnewton678@student.fuhsd.org>
2022-05-19 11:48:53 -0700

    speed balancing

--- std/src/main/java/school/tower/defense/EnemyTypes/LetterOfRec.java
+++ std/src/main/java/school/tower/defense/EnemyTypes/LetterOfRec.java
@@ -9,6 +9,6 @@ import school.tower.defense.Templates.Enemy;
 public class LetterOfRec extends Enemy {
     
     public LetterOfRec(StackPane s, Stage stage, ArrayList<Location> pathLocations, ArrayList<Enemy> enemies) {
-        super("Letter Of Rec", 5, 0.1, 7, s, new String[] {"Enemies/Essay/essay1.png", "Enemies/Essay/essay1.png", "Enemies/Essay/essay2.png", "Enemies/Essay/essay3.png", "Enemies/Essay/essay4.png", "Enemies/Essay/essay5.png"}, stage, pathLocations); 
+        super("Letter Of Rec", 5, 0.15, 7, s, new String[] {"Enemies/Essay/essay1.png", "Enemies/Essay/essay1.png", "Enemies/Essay/essay2.png", "Enemies/Essay/essay3.png", "Enemies/Essay/essay4.png", "Enemies/Essay/essay5.png"}, stage, pathLocations); 
     }
 }
