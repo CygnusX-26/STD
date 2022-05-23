67b520dab50b6b1de847c039b8a83dbff6aabb6d
CygnusX-26 <42686923+CygnusX-26@users.noreply.github.com>
2022-05-22 18:31:33 -0700

    balance

--- std/src/main/java/school/tower/defense/TowerTypes/Dunlap.java
+++ std/src/main/java/school/tower/defense/TowerTypes/Dunlap.java
@@ -16,7 +16,7 @@ public class Dunlap extends Tower {
      * Constructor for the tower
      */
     public Dunlap(Game game, StackPane s, String pathName, Location location) {
-        super(game, s, location, 100, new Upgrade("0", 0, 1, 50, 6), pathName);
+        super(game, s, location, 100, new Upgrade("0", 0, 1, 200, 6), pathName);
         this.s = s;
         this.pathName = pathName;
     }
